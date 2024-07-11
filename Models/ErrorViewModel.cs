0d068dc12abc399cb7919e015ca783cd6676ea19
Owajeul Islam <owajeul01357@gmail.com>
2024-07-11 17:10:21 +0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Task_4_Web_App_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
