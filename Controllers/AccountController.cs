36fb5274916d8da96bfb72899ecb2b58cc3fb3f5
Owajeul Islam <owajeul01357@gmail.com>
2024-07-11 20:57:50 +0600

    add sign up view of accout controller

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Task_4_Web_App_.Models;
+
+namespace Task_4_Web_App_.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult SignIn()
+        {
+            return View();
+        }
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SignUp(UserRegistrationModel userData)
+        {
+            if(!ModelState.IsValid) 
+                return View(userData);
+
+            return RedirectToAction(nameof(SignIn));
+        }
+    }
+}
+
---
+++ Models/UserRegistrationModel.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Task_4_Web_App_.Models
+{
+    public class UserRegistrationModel
+    {
+        [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }  = string.Empty;
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; } = string.Empty;
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Password { get; set; } = string.Empty;
+        [Required]
+        [Display(Name = "Confirm password")]
+        [Compare("Password", ErrorMessage = "Password not matched")]
+        public string ConfirmPassword { get; set; } = string.Empty; 
+
+    }
+}
